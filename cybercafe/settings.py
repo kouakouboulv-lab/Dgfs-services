6d03ae50b79af99af5e34302674bedb4d14f4ef8
broulefrere <kouakouboulv@gmail.com>
2026-05-24 18:46:28 +0000

    Configure PostgreSQL

--- cybercafe/settings.py
+++ cybercafe/settings.py
@@ -3,6 +3,7 @@ Django settings for cybercafe project.
 """
 
 from pathlib import Path
+import dj_database_url
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -71,11 +72,12 @@ WSGI_APPLICATION = 'cybercafe.wsgi.application'
 
 
 # ================= DATABASE =================
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        default='sqlite:///db.sqlite3',
+        conn_max_age=600
+    )
 }
 
 
